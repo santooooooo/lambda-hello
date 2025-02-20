1ff4d1be03d5732df56af9966b2766a254697fac
santooooooo <santoshunsuke@team-lab.com>
2025-02-20 18:36:36 +0900

    テストコードの追加

---
+++ python-test/fizzBuzzTest.py
@@ -0,0 +1,24 @@
+import unittest
+import fizzBuzz
+
+class FizzBuzzTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_normal(self):
+        self.assertEqual(fizzBuzz.fizzbuzz(1), 1)
+
+    def test_fizz(self):
+        self.assertEqual(fizzBuzz.fizzbuzz(3), 'Fizz')
+
+    def test_buzz(self):
+        self.assertEqual(fizzBuzz.fizzbuzz(5), 'Buzz')
+
+    def test_fizz_buzz(self):
+        self.assertEqual(fizzBuzz.fizzbuzz(15), 'FizzBuzz')
+
+if __name__ == '__main__':
+    unittest.main()
